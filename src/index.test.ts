9faaca73097361c9c2b4d9002669de6fcca37177
Spyros Argalias <sargalias@gmail.com>
2019-08-26 15:09:46 +0100

    refactor(deepAssign): Refactor tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,21 +1,16 @@
+import each from 'jest-each';
 import deepAssign from './index'; // eslint-disable-line
 
 describe('deepAssign', () => {
-  test('should return empty object when called with {}, {}', () => {
-    const target = {};
-    const source = {};
-
-    const result = deepAssign(target, source);
-
-    expect(result).toEqual({});
-  });
-
-  test('return object should be the target object', () => {
-    const target = {};
-    const source = {};
-
-    const result = deepAssign(target, source);
-
-    expect(result).toBe(target);
-  });
+  each`
+      target | source | expected
+      ${{}}  | ${{}}  | ${{}}
+  `.test(
+    'deepAssign called with $target and $source should return $expected and should have the same reference as target',
+    ({ target, source, expected }) => {
+      const result = deepAssign(target, source);
+      expect(result).toEqual(expected);
+      expect(result).toBe(target);
+    },
+  );
 });

d510c417ea5ce3d4b2fcc0353f96a5abc20f9432
Spyros Argalias <sargalias@gmail.com>
2019-08-26 16:10:19 +0100

    feat(deepAssign): Disable false positive ESLint lines for no-param-reassign

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,9 @@ const deepAssign: DeepAssign = (target, source) => {
   Object.entries(source).forEach(([key, value]) => {
     if (typeof value === 'object' && value !== null) {
       const newTarget = typeof target[key] === 'object' ? target[key] : {};
-      target[key] = deepAssign(newTarget, value);
+      target[key] = deepAssign(newTarget, value); // eslint-disable-line no-param-reassign
     } else {
-      target[key] = value;
+      target[key] = value; // eslint-disable-line no-param-reassign
     }
   });
   return target;

5db951ec162beec08d040b5452637d2a07f57e84
Spyros Argalias <sargalias@gmail.com>
2019-08-26 14:47:05 +0100

    feat(deepAssign): Reset src files

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,3 @@
-export interface Obj {
-  [k: string]: any;
-}
+const deepAssign = () => {};
 
-const deepCopyObj = (obj?: Obj) => {
-  if (obj === undefined) {
-    return undefined;
-  }
-
-  const result: Obj = {};
-  Object.entries(obj).forEach(([key, value]) => {
-    if (isObjectSubtype(value)) {
-      if (isStrictObject(value)) {
-        result[key] = deepCopyObj(value);
-      }
-    } else {
-      result[key] = value;
-    }
-  });
-  return result;
-};
-
-const isObjectSubtype = (value: any): boolean =>
-  typeof value === 'object' && value !== null;
-
-const isStrictObject = (value: any): boolean => value.constructor === Object;
-
-export { deepCopyObj as default };
+export default deepAssign;

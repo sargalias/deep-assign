601109d3c62ecf340a1f0dfe9848624e952f4d9c
Spyros Argalias <sargalias@gmail.com>
2019-08-26 15:12:24 +0100

    feat(deepAssign): Add case - flat target, flat source

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,9 +3,10 @@ import deepAssign from './index'; // eslint-disable-line
 
 describe('deepAssign', () => {
   each`
-      target | source             | expected
-      ${{}}  | ${{}}              | ${{}}
-      ${{}}  | ${{ a: 1, b: 2 }}  | ${{ a: 1, b: 2 }}
+      target | source                   | expected
+      ${{}}  | ${{}}                    | ${{}}
+      ${{}}  | ${{ a: 1, b: 2 }}        | ${{ a: 1, b: 2 }}
+      ${{ a: 1 }}  | ${{ b: 2, c: 3 }}  | ${{ a: 1, b: 2, c: 3 }}
   `.test(
     'deepAssign called with $target and $source should return $expected and should have the same reference as target',
     ({ target, source, expected }) => {

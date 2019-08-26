7619a0310a2c7c7ed50755886358807b20a39f62
Spyros Argalias <sargalias@gmail.com>
2019-08-26 14:52:18 +0100

    feat(deepAssign): Add case - equality - empty target and source

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,12 @@
 import deepAssign from './index'; // eslint-disable-line
 
 describe('deepAssign', () => {
-  test('empty test', () => {
-    deepAssign();
+  test('should return empty object when called with {}, {}', () => {
+    const target = {};
+    const source = {};
+
+    const result = deepAssign(target, source);
+
+    expect(result).toEqual({});
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-const deepAssign = () => {};
+interface Obj {
+  [k: string]: any;
+}
+
+interface DeepAssign {
+  (target: Obj, source: Obj): Obj;
+}
+
+// eslint-disable-next-line
+const deepAssign: DeepAssign = (target, source) => {
+  return {};
+};
 
 export default deepAssign;

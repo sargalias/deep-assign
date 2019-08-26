86356378682bb0a6bd174de66edfc25d254bcad5
Spyros Argalias <sargalias@gmail.com>
2019-08-26 15:15:21 +0100

    feat(deepAssign): Add case - flat target, nested source

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,19 +1,32 @@
 import each from 'jest-each';
 import deepAssign from './index'; // eslint-disable-line
 
-describe('deepAssign', () => {
+describe('deepAssign, correct result and references', () => {
   each`
       target | source                   | expected
       ${{}}  | ${{}}                    | ${{}}
       ${{}}  | ${{ a: 1, b: 2 }}        | ${{ a: 1, b: 2 }}
       ${{ a: 1 }}  | ${{ b: 2, c: 3 }}  | ${{ a: 1, b: 2, c: 3 }}
       ${{ a: 1 }}  | ${{ a: 2, b: 3 }}  | ${{ a: 2, b: 3 }}
+  }}
   `.test(
-    'deepAssign called with $target and $source should return $expected and should have the same reference as target',
+    'deepAssign called with $target and $source should return $expected',
     ({ target, source, expected }) => {
       const result = deepAssign(target, source);
       expect(result).toEqual(expected);
       expect(result).toBe(target);
     },
   );
+
+  test('flat target with nested source', () => {
+    const target = { a: 1 };
+    const source = { b: 2, c: { d: 4, e: 5 } };
+    const expected = { a: 1, b: 2, c: { d: 4, e: 5 } };
+
+    const result = deepAssign(target, source);
+
+    expect(result).toEqual(expected);
+    expect(result).toBe(target);
+    expect(result.c).not.toBe(source.c);
+  });
 });
--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,14 @@ interface DeepAssign {
 
 // eslint-disable-next-line
 const deepAssign: DeepAssign = (target, source) => {
-  return Object.assign(target, source);
+  Object.entries(source).forEach(([key, value]) => {
+    if (typeof value === 'object') {
+      target[key] = deepAssign({}, value);
+    } else {
+      target[key] = value;
+    }
+  });
+  return target;
 };
 
 export default deepAssign;
